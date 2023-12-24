3f8954278991517bd874c23a37541b052838f855
Ekeshwar Jhode <mayurjhode55@gmail.com>
2023-12-24 13:16:05 +0530

    Closures code

---
+++ Closures.js
@@ -0,0 +1,16 @@
+// Closures
+// A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function's scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time.
+
+function process(){
+    let i=0;
+    function innerProcess(){
+        i+=1;
+        return i;
+    }
+    return innerProcess;
+}
+let res = process();
+console.log(res);
+console.log("Calling first time ",res());
+console.log("Calling Second time ",res());
+console.log("Calling Third time ",res());
